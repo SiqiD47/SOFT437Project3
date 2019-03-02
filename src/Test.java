a13a36f4229136eddb79e01b7e1a1f7d3f625b6e
Siqi Dai <siqidai97@gmail.com>
2019-03-02 10:32:43 -0500

    0302

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,7 @@ public class Test {
 		int[] arraySize = { 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 6000, 8000, 10000 };
 		for (int i = 0; i < arraySize.length; i++)
 			fourSortingAlgs(ins, arraySize[i]);
-		
+
 		ins.stopTiming("main");
 		ins.dump("TestDriveSec");
 
@@ -25,47 +25,48 @@ public class Test {
 		return array;
 	}
 
-	public static int[] bubbleSort(Instrumentation ins, int[] array) {
+	public static void bubbleSort(Instrumentation ins, int[] array) {
 		ins.startTiming("bubbleSort()");
 		BubbleSort2Algorithm b = new BubbleSort2Algorithm();
 		b.sort(array);
 		ins.stopTiming("bubbleSort()");
-		return array;
 	}
 
-	public static int[] quickSort(Instrumentation ins, int[] array) {
+	public static void quickSort(Instrumentation ins, int[] array) {
 		ins.startTiming("quickSort()");
 		NaiveQuickSortAlgorithm b = new NaiveQuickSortAlgorithm();
 		b.sort(array);
 		ins.stopTiming("quickSort()");
-		return array;
 	}
 
-	public static int[] mergeSort(Instrumentation ins, int[] array) {
+	public static void mergeSort(Instrumentation ins, int[] array) {
 		ins.startTiming("mergeSort()");
 		ExtraStorageMergeSortAlgorithm b = new ExtraStorageMergeSortAlgorithm();
 		b.sort(array);
 		ins.stopTiming("mergeSort()");
-		return array;
 	}
 
-	public static int[] selectionSort(Instrumentation ins, int[] array) {
+	public static void selectionSort(Instrumentation ins, int[] array) {
 		ins.startTiming("selectionSort()");
 		SelectionSortAlgorithm b = new SelectionSortAlgorithm();
 		b.sort(array);
 		ins.stopTiming("selectionSort()");
-		return array;
 	}
 
 	// test method for Question 3 & 4
 	public static void fourSortingAlgs(Instrumentation ins, int arraySize) {
 		ins.comment("array size: " + arraySize);
 		ins.startTiming("fourSortingAlgs()");
-		int[] a = populateArray(ins, arraySize);
-		int[] bubble = bubbleSort(ins, a);
-		int[] quick = quickSort(ins, a);
-		int[] merge = mergeSort(ins, a);
-		int[] selection = selectionSort(ins, a);
+
+		int[] a1 = populateArray(ins, arraySize);
+		int[] a2 = a1.clone();
+		int[] a3 = a1.clone();
+		int[] a4 = a1.clone();
+		bubbleSort(ins, a1);
+		quickSort(ins, a2);
+		mergeSort(ins, a3);
+		selectionSort(ins, a4);
+
 		ins.stopTiming("fourSortingAlgs()");
 	}
 
